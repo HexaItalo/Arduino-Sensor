266ed20737ef2f6d76b4e775b43e4f7114733f05
Luis Fernando <167149337+LoiusBorges@users.noreply.github.com>
2025-04-28 23:39:02 -0300

    Add files via upload

---
+++ ArduinoSensor.ino
@@ -0,0 +1,50 @@
+const int sensorGas = A0;
+const int limitGas = 500;
+const int sensorFlame = A1;
+const int limitFlame = 500;
+const int ledGas = 2;
+const int ledFlame = 3;
+const int buzzer = 4;
+
+void setup() {
+  pinMode(ledGas, OUTPUT);
+  pinMode(ledFlame, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void gasDetector(){
+  int gasRead = analogRead(sensorGas);
+
+  if(gasRead >= limitGas){
+    Serial.println("Gás detectado! ⚠️");
+    Serial.print("Valor lido MQ-09: ");
+    Serial.println(gasRead);
+
+    digitialWrite(buzzer, HIGH);
+    digitalWrite(ledGas, HIGH);
+    delay(3000);
+  }
+}
+
+void flameDectetor(){
+  int flameRead = analogRead(sensorFlame);
+
+  if(flameRead <= limitFlame){
+    Serial.println("Fogo detectado! 🔥");
+    Serial.print("Valor lido Flame: ");
+    Serial.println(flameRead);
+
+    digitalWrite(buzzer, HIGH);
+    digitalWrite(ledFlame, HIGH);
+    delay(3000) ;   
+  }
+}
+
+void loop() {
+  gasDectetor();
+  gasDectetor();  
+
+  delay(3000);
+}
